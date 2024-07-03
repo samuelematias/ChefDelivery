6b78ace7f53ae4b395a4e640d24791888f7e2678
Samuel Matias <hi@samuelematias.com>
2024-07-03 11:13:46 -0300

    init project

---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Samuel Elias Silva Matias on 03/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Text1")
+            Text("Text2")
+        }
+    }
+}
+
+#Preview {
+    ContentView().previewLayout(.sizeThatFits)
+}
