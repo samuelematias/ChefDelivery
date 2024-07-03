7adf31a6d87e70a91b54fd278cb7cfee10d92786
Samuel Matias <hi@samuelematias.com>
2024-07-03 12:08:57 -0300

    feat: finish the NavigationBar implementation

--- ChefDelivery/NavigationBaar/NavigationBar.swift
+++ ChefDelivery/NavigationBaar/NavigationBar.swift
@@ -10,16 +10,29 @@ import SwiftUI
 struct NavigationBar: View {
     var body: some View {
         HStack{
+            Spacer()
+            
             Button("R. Vergueiro, 3185") {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
-            Button("Notificações"){
-                
-            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "bell.badge")
+                    .font(.title3)
+                    .foregroundColor(.red)
+            })
+            
         }
     }
 }
 
 #Preview {
     NavigationBar()
+        .previewLayout(.sizeThatFits)
+        .padding()
 }
