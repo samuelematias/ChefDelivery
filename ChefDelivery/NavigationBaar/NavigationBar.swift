cb746cdc8f652884bd5bdfee4d27611374318e26
Samuel Matias <hi@samuelematias.com>
2024-07-03 11:50:12 -0300

    feat: starting create the NavigationBar

---
+++ ChefDelivery/NavigationBaar/NavigationBar.swift
@@ -0,0 +1,25 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by Samuel Elias Silva Matias on 03/07/24.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        HStack{
+            Button("R. Vergueiro, 3185") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Button("Notificações"){
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
