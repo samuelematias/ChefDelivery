0208a9ad4cd1de7f615aacd31e179b27431e2964
Samuel Matias <hi@samuelematias.com>
2024-07-08 13:15:04 -0300

    feat: improved the OrderTypeGridView component

--- ChefDelivery/GridView/OrderTypeGridView.swift
+++ ChefDelivery/GridView/OrderTypeGridView.swift
@@ -8,15 +8,20 @@
 import SwiftUI
 
 struct OrderTypeGridView: View {
+    
+    var gridLayout: [GridItem]{
+        return Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
+    }
+
     var body: some View {
-        LazyHGrid(rows: [
-            GridItem(.fixed(100)),
-            GridItem(.fixed(100))
-        ], content: {
-            ForEach(ordersMock){
-                orderItem in Text(orderItem.name)
+        LazyHGrid(rows: gridLayout, spacing: 15 , content: {
+            ForEach(ordersMock) {
+               orderItem in OrderTypeView(orderType: orderItem)
             }
         })
+        .frame(height: 200)
+        .padding(.horizontal, 15)
+        .padding(.top, 15)
     }
 }
 
