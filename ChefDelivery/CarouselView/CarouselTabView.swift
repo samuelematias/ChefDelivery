5e7bd4014d499da4b433757abfff36d311c36a53
Samuel Matias <hi@samuelematias.com>
2024-07-08 14:02:42 -0300

    feat: implement CarouselTabView component

---
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -0,0 +1,29 @@
+//
+//  CarouselTabView.swift
+//  ChefDelivery
+//
+//  Created by Samuel Elias Silva Matias on 08/07/24.
+//
+
+import SwiftUI
+
+struct CarouselTabView: View {
+    
+    let ordersMock: [OrderType] = [
+        OrderType(id: 1, name: "barbecue-banner", image: "barbecue-banner"),
+        OrderType(id: 2, name: "brazilian-meal-banner", image: "brazilian-meal-banner"),
+        OrderType(id: 3, name: "pokes-banner", image: "pokes-banner"),
+    ]
+    
+    var body: some View {
+        TabView {
+            ForEach(ordersMock){
+               mock in CarouselItemView(order: mock)
+            }
+        }
+    }
+}
+
+#Preview {
+    CarouselTabView()
+}
