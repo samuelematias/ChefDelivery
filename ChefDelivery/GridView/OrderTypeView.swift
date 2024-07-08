e8be207c8f42e7b4e8bfe1c3ac204f4db5a4bda2
Samuel Matias <hi@samuelematias.com>
2024-07-08 12:46:26 -0300

    feat: implement OrderTypeView component

---
+++ ChefDelivery/GridView/OrderTypeView.swift
@@ -0,0 +1,30 @@
+//
+//  OrderTypeView.swift
+//  ChefDelivery
+//
+//  Created by Samuel Elias Silva Matias on 08/07/24.
+//
+
+import SwiftUI
+
+struct OrderTypeView: View {
+    
+    let orderType: OrderType
+    
+    var body: some View {
+        VStack(spacing: 5){
+            Image(orderType.image)
+                .resizable()
+                .scaledToFit()
+                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+            Text(orderType.name)
+                .font(.system(size: 10))
+        }
+        .frame(width: 70, height: 100)
+    }
+}
+
+#Preview {
+    OrderTypeView(orderType: OrderType(id: 1, name: "Restaurantes", image: "hamburguer"))
+        .previewLayout(.sizeThatFits)
+}
